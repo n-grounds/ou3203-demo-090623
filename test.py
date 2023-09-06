367ca076595ccb443e561b59a913d8a07a461635
Nic Grounds <nicgrounds@gmail.com>
2023-09-06 17:35:34 -0500

    Add two more tests

--- test.py
+++ test.py
@@ -10,3 +10,13 @@ if app.is_palindrome( 'dog' ):
     print('Test failed')
 else:
     print('Test passed')
+
+if app.is_palindrome( 'robber' ):
+    print('Test failed')
+else:
+    print('Test passed')
+
+if app.is_palindrome( 'i' ):
+    print('Test passed')
+else:
+    print('Test failed')
