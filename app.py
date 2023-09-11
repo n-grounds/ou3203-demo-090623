b266a9d44f92414ff7228f150ec2a4846e94d391
Nic Grounds <nicgrounds@gmail.com>
2023-09-11 16:53:28 -0500

    Fix is_palindrome and add compiled file to .gitignore

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 def is_palindrome(string):
     for i, c in enumerate(string):
-        if string[i] == string[len(string)-i-1]:
-            return True
-    return False
+        if string[i] != string[len(string)-i-1]:
+            return False
+    return True
